5b740924f3d7dc4aaef49499bdbb06e1488a01d0
veengo <vythithuthuy@gmail.com>
2017-08-14 18:11:49 +0700

    C7

---
+++ Homework/C7/C7-E2/C7-E2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C7E2
+{
+	class MainClass
+	{
+		
+			//Greatest Common Divisor 
+			static void Main()
+			{
+				Console.WriteLine(GCD(new[] { 10, 15, 30, 45 }));
+			}
+
+			static int GCD(int a, int b)
+			{
+				return b == 0 ? a : GCD(b, a % b);
+			}
+
+			static int GCD(int[] integerSet)
+			{
+			return integerSet.Aggregate (GCD);
+			}  
+		}
+	}
---
+++ Homework/C7/C7/Ex 1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C7
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//decimal to binary 
+			int value = 13;
+			string binary = Convert.ToString (value, 2);
+			Console.WriteLine (binary);
+		}
+			 
+		}
+	}
