eb66e9159c3bb6bc61568ac6d77d69c27f84123a
veengo <vythithuthuy@gmail.com>
2017-08-14 19:44:12 +0700

    C7-E2

--- Homework/C7/C7-E2/C7-E2/Program.cs
+++ Homework/C7/C7-E2/C7-E2/Program.cs
@@ -4,21 +4,19 @@ namespace C7E2
 {
 	class MainClass
 	{
-		
-			//Greatest Common Divisor 
-			static void Main()
-			{
-				Console.WriteLine(GCD(new[] { 10, 15, 30, 45 }));
-			}
 
-			static int GCD(int a, int b)
+			public static void Main (string[] args)
 			{
-				return b == 0 ? a : GCD(b, a % b);
+			int a,b;
+			a = 7;
+			b = 9;
+			i = (a > b) ? a : b;
+			int GCD = 1;
+			for (int j = 1; j <= i; j++) {
+				if (a % j == 0 && b % j == 0) {
+					GCD = j;
+				}
+				Console.WriteLine (GCD);
 			}
-
-			static int GCD(int[] integerSet)
-			{
-			return integerSet.Aggregate (GCD);
-			}  
 		}
 	}
